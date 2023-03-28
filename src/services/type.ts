7902590d0587ab0de8b6d30beb199f960d2e976c
Ashish Jha <ashishjha14411@gmail.com>
2023-03-28 20:37:34 +0530

    added types for the api data

---
+++ src/services/type.ts
@@ -0,0 +1,25 @@
+export type productsData = [
+    {
+      id?: number | undefined,
+      title?: string | undefined,
+      description?: string | undefined,
+      price?: number | undefined,
+      discountPercentage?: number | undefined,
+      rating?: number | undefined,
+      stock?: number| undefined,
+      brand?: string | undefined,
+      category?: string | undefined,
+      thumbnail?: string | undefined,
+      images?:string[] | undefined
+      quantity?: number | 0
+    }
+  ]
+  
+  
+  export interface JsonObj {
+      products: productsData[]
+      total: number,
+      skip: number,
+      limit: number
+  }
+  
