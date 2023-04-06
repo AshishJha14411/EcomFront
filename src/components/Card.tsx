797d44361b7a44834294b8e5f6ef7621bc24b7f9
Ashish Jha <ashishjha14411@gmail.com>
2023-04-07 04:13:12 +0530

    fixed ui at 4 breakpoints

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -10,11 +10,11 @@ const Card = ({ customdata }: { customdata: productsData }) => {
    const dispatch = useAppDispatch()
    console.log(products)
    return (
-      <div className='w-[100%] p-4 flex cursor-pointer flex-wrap flex-col mx-auto lg:flex-row items-center' >
+      <div className='w-[100%]  p-4 flex cursor-pointer flex-wrap flex-col mx-auto lg:flex-row lg:justify-around items-center' >
          {customdata && customdata.map((item) => {
             return (
-               <div className='bg-[#FFFFFF] p-[2rem]' key={item.id} >
-                 {/*  <div onClick={() => {
+               <div className='bg-[#FFFFFF] p-[2rem] text-center m-[1rem] lg:w-[30%] 2xl:w-[20%]' key={item.id} >
+                  <div onClick={() => {
                   dispatch(prodId(item.id))
                   navigate('/product')
                   dispatch(category(''))
@@ -24,15 +24,15 @@ const Card = ({ customdata }: { customdata: productsData }) => {
                }}>
 
                      <div>
-                        <h1 className='font-bold text-2xl'>{item.title}</h1>
+                        <h1 className='font-bold text-2xl lg:text-xl'>{item.title}</h1>
                         <p className=' text-xl'><span className='text-[#e35a38] font-extrabold'>Price: </span>${item.price}</p>
                      </div>
-                     <img src={item.images[0]} alt={item.title} className='w-[80%] pt-9 mx-auto' />
+                     <img src={item.images[0]} alt={item.title} className='w-[100%] pt-9 mx-auto' />
                   </div>
-                  <div className='w-[100%] flex flex-row justify-between text-xl pt-9'>
+                  <div className='w-[100%] flex flex-row justify-between text-xl lg:text-lg pt-9'>
                      <h2 className='text-[#e35a38] font-bold cursor-pointer hover:text-[#000000]' onClick={() => dispatch(addToCart(item))}>Add to Cart</h2>
                      <p className='hover:text-[#e35a38] text-[#000000] cursor-pointer' onClick={() => dispatch(removeItem(item.id))}>Explore More</p>
-                  </div> */}
+                  </div>
                </div>
             )
          })}
